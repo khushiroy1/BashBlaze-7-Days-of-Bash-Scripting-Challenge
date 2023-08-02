a2665dd267ac180d51427b9a73ede79705691757
khushiroy1 <khushiroy407@gmail.com>
2023-08-02 03:05:17 +0000

    Day_1 challenge completed

---
+++ my_solutions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# First line is shababng
+# Task 1
+#this is ,y first scrip in this challenge where I will prin helllow world through echo
+
+
+# Task 2 --> I am using echo command to display message on terminal.
+echo "Hellow world"
+
+
+# Task 3--> Declaring variables and assigning value to them
+
+Firstname=khushi
+Lastname=Roy
+
+
+# Task 4-->  Using declared variable
+echo "my name is $Firstname $Lastname"
+
+# Task 5-->
+
+echo "path for Bash == $BASH "
+
+echo "Process id for current instance ==  $$"
+
+echo "group id for current user == $GROUPS"
+
+
+# Task 6 Wildcards
+
+
+echo "Files with .txt extension in the current directory:"
+ls *.txt
+
